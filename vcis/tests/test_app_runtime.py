f387fd5fb9ce72fb9329d90534b866435e931647
zong <zong@globalblue.com>
2020-09-24 10:43:12 +0800

    update test

--- vcis/tests/test_app_runtime.py
+++ vcis/tests/test_app_runtime.py
@@ -2,9 +2,9 @@ import unittest
 
 class app_runtime_tests(unittest.TestCase):
     
-    def get_host_name_test(self):
+    def test_get_host_name(self):
         """
-        python -m unittest tests.test_app_runtime.app_runtime_tests.get_host_name_test
+        python -m unittest tests.test_app_runtime.app_runtime_tests.test_get_host_name
         """
         # Arrange
         from helpers.app_runtime import get_host_name
